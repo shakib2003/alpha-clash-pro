845eae733297d1080bf7ba48b07b0c1ea806fb1e
shakib2003 <shakib2003skb@gmail.com>
2024-02-15 01:08:07 +0600

    key show by javascript

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -5,7 +5,21 @@
 //   playGroundSection.classList.remove('hidden');
 // }
 
+// function continueGame(){
+//   const alphabet = getARandomAlphabet();
+//   console.log('alphabet', alphabet);
+// }
+
+function continueGame(){
+  const alphabet = getARandomAlphabet();
+  console.log('alphabet', alphabet);
+  const getAlphabet = document.getElementById('current-alphabet');
+  getAlphabet.innerText = alphabet;
+  setBackgroundColorById(alphabet);
+}
+
 function play(){
   hideElementById('home-screen');
   showElementById('play-ground');
+  continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,27 @@ function showElementById(elementId){
   const element = document.getElementById(elementId);
   element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-orange-400');
+}
+
+// function getARandomAlphabet(){
+//   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//   const alphabets = alphabetString.split('');
+
+//   const randomNumber = Math.random()*25;
+//   const index = Math.round(randomNumber);
+//   const alphabet = alphabets[index];
+//   return alphabet;
+// }
+
+function getARandomAlphabet(){
+  const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabets = alphabetString.split('');
+  const randomNumber = Math.random()*25;
+  const index = Math.round(randomNumber);
+  const alphabet = alphabets[index];
+  return alphabet;
+}
