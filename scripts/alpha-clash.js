24b993d4dcca45ac0c6e0d009b9c1c75e03398e2
shakib2003 <shakib2003skb@gmail.com>
2024-02-14 20:23:18 +0600

    playgroun, score section added and make interactive

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,11 @@
+// function play(){
+//   const homeSection = document.getElementById('home-screen');
+//   homeSection.classList.add('hidden');
+//   const playGroundSection = document.getElementById('play-ground');
+//   playGroundSection.classList.remove('hidden');
+// }
+
+function play(){
+  hideElementById('home-screen');
+  showElementById('play-ground');
+}
